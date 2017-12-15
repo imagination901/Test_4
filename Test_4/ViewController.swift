eae8909ea427b89e98b45c6b199c9a02f10d73d0
Bohdan Yavorskyy <bohdanyavorskyy@Bohdans-MacBook-Pro.local>
2017-12-15 19:32:55 +0400

    added print statement

--- Test_4/ViewController.swift
+++ Test_4/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("123")
+
     }
 
     override func didReceiveMemoryWarning() {
